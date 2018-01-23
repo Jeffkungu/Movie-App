603a7dbb2807867316165112831b1700871f2470
Jeffkungu <Jeffkungu@gmail.com>
2018-01-23 17:34:41 +0300

    [Feature] adds a preference screen

--- app/src/main/java/com/example/jeffkungu/movieapp/SettingsActivity.java
+++ app/src/main/java/com/example/jeffkungu/movieapp/SettingsActivity.java
@@ -1,8 +1,26 @@
 package com.example.jeffkungu.movieapp;
 
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+
 /**
  * Created by Jeffkungu on 10/11/2017.
  */
 
-public class SettingsActivity {
+public class SettingsActivity extends PreferenceActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.prefrences);
+        }
+    }
 }
