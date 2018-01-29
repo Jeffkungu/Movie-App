0c61d82803560e666c70e5689716f9a0fe7ee88d
Jeffkungu <Jeffkungu@gmail.com>
2018-01-29 12:08:07 +0300

    [Feature] adds a new model for the response to the MovieTrailer Api call

---
+++ app/src/main/java/com/example/jeffkungu/movieapp/model/TrailerResponse.java
@@ -0,0 +1,33 @@
+package com.example.jeffkungu.movieapp.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by Jeffkungu on 25/01/2018.
+ */
+
+public class TrailerResponse {
+
+    @SerializedName("id")
+    private int id_trailer;
+    @SerializedName("results")
+    private List<Trailer> results;
+
+    public int getIdTrailer() {
+        return id_trailer;
+    }
+
+    public void setIdTrailer(int id_trailer) {
+        this.id_trailer = id_trailer;
+    }
+
+    public List<Trailer> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Trailer> results) {
+        this.results = results;
+    }
+}
