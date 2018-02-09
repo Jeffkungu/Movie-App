8aea6f4a4408ab1d804f9a175d7d9c6d3ada4df7
Jeffkungu <Jeffkungu@gmail.com>
2018-02-09 10:21:14 +0300

    [Feature] adds movie trailer Url

--- app/src/main/java/com/example/jeffkungu/movieapp/api/Service.java
+++ app/src/main/java/com/example/jeffkungu/movieapp/api/Service.java
@@ -1,9 +1,11 @@
 package com.example.jeffkungu.movieapp.api;
 
 import com.example.jeffkungu.movieapp.model.MoviesResponse;
+import com.example.jeffkungu.movieapp.model.TrailerResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 /**
@@ -15,9 +17,17 @@ import retrofit2.http.Query;
 public interface Service {
 
     // The @GET annotation makes it possible to perform HTTP calls to the GET API-endpoint
-    @GET("movie/popular")
-    Call<MoviesResponse> getPopularMovies(@Query("api_key") String apiKey);
+    // The base url for movie api is: https://api.themoviedb.org
 
+    // The url for movie api (get popular movies) is: https://api.themoviedb.org/3/movie/popular?api_key="The Api key"
+    @GET("movie/popular")
+    Call<MoviesResponse>  getPopularMovies(@Query("api_key") String apiKey);
+
+    // The url for movie api (get top_rated movies) is: https://api.themoviedb.org/3/movie/top_rated?api_key="The Api key"
     @GET("movie/top_rated")
     Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+
+    // The url for a single movie-trailer in the movie api is: https://api.themoviedb.org/3/movie/movie_id/videos?api_key="The Api key"
+    @GET("movie/{movie_id}/videos")
+    Call<TrailerResponse> getMovieTrailer(@Path("movie_id") int id, @Query("api_key") String apiKey);
 }
